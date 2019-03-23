961f20efb4f8d4797a1ff88ed71331445b3e2ae3
Alexander Seyss <a@seyss.at>
2019-03-23 23:04:34 +0100

    new and create for kid

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,2 +1,21 @@
 class KidsController < ApplicationController
+
+  def new
+    @kid = Kid.new
+  end
+
+  def create
+    @kid = Kid.new(kid_params)
+    if @kid.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def kid_params
+    params.require(:kid).permit(:name, :photo, :user_id)
+  end
 end
