eddaf28ba87802d29a353ba198547265b1e7ecab
Alexander Seyss <a@seyss.at>
2019-03-23 19:41:16 +0100

    new create method for events, new view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,22 @@
 class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:animal).permit(:title, :date, :place, :photo, :description, :kid_id, :user_id)
+  end
 end
