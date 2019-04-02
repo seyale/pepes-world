9c1b0d5af0e650bafb5f5572a0a350b436373704
Alexander Seyss <a@seyss.at>
2019-04-02 10:36:30 +0200

    order descending on event index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @events = Event.all.order(date: :asc)
+    @events = Event.all.order(date: :desc)
   end
 end
