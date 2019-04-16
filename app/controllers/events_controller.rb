eb97acb0c7e301adbd1bca8c2a62f93fce02aa1f
Alexander Seyss <a@seyss.at>
2019-04-16 15:08:50 +0200

    events page started

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,10 @@ class EventsController < ApplicationController
 
   before_action :set_event, only: [:show]
 
+  def index
+    @events = Event.all
+  end
+
   def new
     @event = Event.new
     @all_kids = Kid.all
