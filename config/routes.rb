6cbbb7a9a1955c7e5d4ddbf8a26ffcc2e6691752
Alexander Seyss <a@seyss.at>
2019-04-21 09:07:24 +0200

    added tabs to index events & changed root route to events index for now

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :events
   resources :kids
 
-  root to: 'pages#home'
+  #root to: 'pages#home'
+  root to: 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
